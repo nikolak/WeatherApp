1c766712658427c4d18175cd483e6b90c5520555
Nikola Kovacevic <nikolak@outlook.com>
2015-02-26 22:26:48 +0100

    Initial commit

--- app/src/main/java/com/nikolak/weatherapp/ForecastIO/Daily.java
+++ app/src/main/java/com/nikolak/weatherapp/ForecastIO/Daily.java
@@ -1,4 +1,34 @@
 package com.nikolak.weatherapp.ForecastIO;
 
+import java.util.List;
+import com.nikolak.weatherapp.ForecastIO.Day;
+
 public class Daily {
+    private List<Day> dayData;
+    private String icon;
+    private String summary;
+
+    public List getData() {
+        return this.dayData;
+    }
+
+    public void setData(List<Day> data) {
+        this.dayData = data;
+    }
+
+    public String getIcon() {
+        return this.icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getSummary() {
+        return this.summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
 }
--- app/src/main/java/com/nikolak/weatherapp/ForecastIO/Hourly.java
+++ app/src/main/java/com/nikolak/weatherapp/ForecastIO/Hourly.java
@@ -1,4 +1,34 @@
 package com.nikolak.weatherapp.ForecastIO;
 
+import java.util.List;
+import com.nikolak.weatherapp.ForecastIO.Hour;
+
 public class Hourly {
+    private List<Hour> hourData;
+    private String icon;
+    private String summary;
+
+    public List getData() {
+        return this.hourData;
+    }
+
+    public void setData(List data) {
+        this.hourData = data;
+    }
+
+    public String getIcon() {
+        return this.icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getSummary() {
+        return this.summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
 }
