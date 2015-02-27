ec6d39b139c9e539594b884a3dba8f2892c3fc88
Nikola Kovacevic <nikolak@outlook.com>
2015-02-27 09:49:59 +0100

    Add minutely parsing

--- app/src/main/java/com/nikolak/weatherapp/ForecastIO/Forecast.java
+++ app/src/main/java/com/nikolak/weatherapp/ForecastIO/Forecast.java
@@ -2,11 +2,14 @@ package com.nikolak.weatherapp.ForecastIO;
 
 
 
+import android.util.Log;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Forecast {
     public Currently currently = new Currently();
+    public Minutely minutely = new Minutely();
     public Hourly hourly = new Hourly();
     public Daily daily = new Daily();
 
@@ -20,6 +23,17 @@ public class Forecast {
 
         JSONObject currentlyJObject = response.getJSONObject("currently");
         this.currently.ConstructFromJson(currentlyJObject);
+
+        try{
+            JSONObject minutelyJObject = response.getJSONObject("minutely");
+            if (minutelyJObject!=null){
+                minutely.constructFromJson(minutelyJObject);
+            } else{
+                minutely = null;
+            }
+        } catch (JSONException e){
+            Log.d("Json", "Minutely not available");
+        }
 
         JSONObject hourlyJObject = response.getJSONObject("hourly");
         this.hourly.constructFromJson(hourlyJObject);
